e7437fa90eef7353b55181cd59d360790a163015
Humberto <colmenareshr@gmail.com>
2024-11-14 14:11:46 -0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./**/*.php'],
-  darkMode: false,
+  content: ["./**/*.php", "./src/**/*.js", "./src/**/*.css"],
   theme: {
     extend: {
       colors: {
-        primary: '#1e3a8a', // Azul principal
-        secondary: '#4b5563', // Cinza principal
+        primary: "#1e3a8a", // Azul principal
+        secondary: "#4b5563", // Cinza principal
       },
     },
   },
-  variants: {},
   plugins: [],
 };
